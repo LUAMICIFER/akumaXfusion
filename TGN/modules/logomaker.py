ba953725d306280de4169acea2611fd6be99cb18
LUAMICIFER <96227537+LUAMICIFER@users.noreply.github.com>
2022-05-28 05:42:20 +0530

    Update logomaker.py

--- TGN/modules/logomaker.py
+++ TGN/modules/logomaker.py
@@ -31,7 +31,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "Emli.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made by logomaker")
+    await tbot.send_file(event.chat_id, fname2, caption="Made by @DAISHINYUUROBOT")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -65,7 +65,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "Emli.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By logomaker")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @DAISHINYUUROBOT")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
